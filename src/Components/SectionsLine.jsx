1ec7a07a35743a6933cf276056e3c56f1e0cde18
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-03-30 01:14:03 +0530

    77

--- src/Components/SectionsLine.jsx
+++ src/Components/SectionsLine.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SectionsLine = () => {
   return (
-    <div className="bg-[#969696] w-[80vw] max-w-[1200px] h-[5px] md:h-[7px] mt-[30px] mb-[60px] ml-[8%] mx-auto rounded-full md:w-[92vw]" />
+    <div className="bg-[#969696] w-[77vw] max-w-[1200px] h-[5px] md:h-[7px] mt-[30px] mb-[60px] ml-[8%] mx-auto rounded-full md:w-[92vw]" />
   );
 };
 
