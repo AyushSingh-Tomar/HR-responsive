bab636865b8b9fe0b870b5d87d9fe7f804f92c6f
Ayush Singh Tomar <ayushtomarrudra@gmail.com>
2025-03-31 19:29:30 +0530

    clickable link

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -70,7 +70,7 @@ const ContactForm = () => {
   <p>info@thehrcompany.co.in</p>
   <br />
   <p className="break-words whitespace-break-spaces">
-    https://www.linkedin.com/in/udai-upendra-6514895/
+     <a href="https://www.linkedin.com/in/udai-upendra-6514895/">https://www.linkedin.com/in/udai-upendra-6514895/</a>
   </p>
 </div>
 
